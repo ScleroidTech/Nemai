f1e7a1bf29bb38fde1d1a9796e580356163236c5
mitalipadiya <mdpadiya10@gmail.com>
2017-09-19 19:04:20 +0530

    Auto Complete view changed. app crashes when value selected due toon data set changed. complete UI overhaul.

--- app/src/main/java/info/androidhive/navigationdrawer/ServerConstants.java
+++ app/src/main/java/info/androidhive/navigationdrawer/ServerConstants.java
@@ -9,6 +9,7 @@ public class ServerConstants {
     public class serverUrl {
 
         public static final String POST_COURIER = "http://ec2-18-220-38-112.us-east-2.compute.amazonaws.com:3002/insert";
+        public static final String POST_PINCODE = "http://ec2-18-221-108-81.us-east-2.compute.amazonaws.com:3008/insertquery";
 
 
 
@@ -16,6 +17,7 @@ public class ServerConstants {
 
     public class ServiceCode {
         public static final int POST_COURIER = 101;
+        public static final int POST_PINCODE = 102;
     }
 
 
