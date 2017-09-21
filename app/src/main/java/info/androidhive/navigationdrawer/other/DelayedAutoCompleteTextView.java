3399c934aa6bed62163fe22f1b1f33f149d64f3a
Ganesh Kaple <gvklive@gmail.com>
2017-09-21 16:32:55 +0530

    Pushing pincode, working on login & register,

--- app/src/main/java/info/androidhive/navigationdrawer/other/DelayedAutoCompleteTextView.java
+++ app/src/main/java/info/androidhive/navigationdrawer/other/DelayedAutoCompleteTextView.java
@@ -15,16 +15,20 @@ public class DelayedAutoCompleteTextView extends android.support.v7.widget.AppCo
     /* Custom View of AutocompleteTextView which helps load the textview after a few keystrokes,
     to prevent fewer network calles & avoid invalid data - */
 
-    private static final int MESSAGE_TEXT_CHANGED = 1;
+    private static final int MESSAGE_TEXT_CHANGED = 0;
     private static final int DEFAULT_AUTOCOMPLETE_DELAY = 1;
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
+
             DelayedAutoCompleteTextView.super.performFiltering((CharSequence) msg.obj, msg.arg1);
+
+
         }
     };
     private int mAutoCompleteDelay = DEFAULT_AUTOCOMPLETE_DELAY;
     private ProgressBar mLoadingIndicator;
+
 
     public DelayedAutoCompleteTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -38,13 +42,14 @@ public class DelayedAutoCompleteTextView extends android.support.v7.widget.AppCo
         mAutoCompleteDelay = autoCompleteDelay;
     }
 
+
     @Override
     protected void performFiltering(CharSequence text, int keyCode) {
         if (mLoadingIndicator != null) {
             mLoadingIndicator.setVisibility(View.VISIBLE);
         }
         mHandler.removeMessages(MESSAGE_TEXT_CHANGED);
-        mHandler.sendMessageDelayed(mHandler.obtainMessage(MESSAGE_TEXT_CHANGED, text), mAutoCompleteDelay);
+        mHandler.sendMessage(mHandler.obtainMessage(MESSAGE_TEXT_CHANGED, text));
     }
 
     @Override
@@ -52,6 +57,8 @@ public class DelayedAutoCompleteTextView extends android.support.v7.widget.AppCo
         if (mLoadingIndicator != null) {
             mLoadingIndicator.setVisibility(View.GONE);
         }
+
+
         super.onFilterComplete(count);
     }
 
