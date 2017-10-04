f5b60577d2d7af171ee214dec395674c1ab945c6
Ganesh Kaple <gvklive@gmail.com>
2017-10-04 19:17:17 +0530

    CHanged Package name & other details, Implemented database, #TODO Interconnection between activities

---
+++ app/src/main/java/com/scleroid/nemai/models/PinCode.java
@@ -0,0 +1,52 @@
+package com.scleroid.nemai.models;
+
+/**
+ * Created by scleroid on 15/9/17.
+ */
+
+public class PinCode {
+
+    private String location, pincode, state, area;
+
+    public PinCode(String pincode, String location, String state,String area) {
+        this.location = location;
+        this.pincode = pincode;
+        this.state =state;
+        this.area = area;
+
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+
+}
