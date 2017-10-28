c2d2aed75b6c4417607426f3dee7e2b59a3ca3d8
Ganesh Kaple <gvklive@gmail.com>
2017-10-28 17:16:13 +0530

    changes made to handler of delayedautocompleteview, made it non static, coz itwasgiving an issue with only one data setgetting loaded.
    Signed-off-by: Ganesh Kaple <gvklive@gmail.com>

--- app/src/main/java/com/scleroid/nemai/activity/TestDBActivity.java
+++ app/src/main/java/com/scleroid/nemai/activity/TestDBActivity.java
@@ -6,21 +6,24 @@ import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.scleroid.nemai.R;
+import com.scleroid.nemai.adapter.PinAutoCompleteAdapter;
 import com.scleroid.nemai.adapter.PinDatabaseHelper;
 import com.scleroid.nemai.models.PinCode;
+import com.scleroid.nemai.other.DelayedAutoCompleteTextView;
 
 import java.io.IOException;
 import java.util.List;
+
+import static com.scleroid.nemai.fragment.HomeFragment.THRESHOLD;
 
 
 public class TestDBActivity extends Activity {
     private final static String TAG = "MainActivity";
     PinDatabaseHelper dbHelper = null;
-    EditText editText;
+    DelayedAutoCompleteTextView editText, editText2;
     TextView textView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +38,25 @@ public class TestDBActivity extends Activity {
         }
         textView = findViewById(R.id.bodytext);
         editText = findViewById(R.id.test_input);
+        editText.setThreshold(THRESHOLD);
+        PinAutoCompleteAdapter pinAutoCompleteAdapter1 = new PinAutoCompleteAdapter(getApplicationContext());
+        pinAutoCompleteAdapter1.notifyDataSetChanged();
+        editText.setAdapter(pinAutoCompleteAdapter1);
+        editText2 = findViewById(R.id.test_input2);
+        editText2.setThreshold(THRESHOLD);
+        PinAutoCompleteAdapter pinAutoCompleteAdapter = new PinAutoCompleteAdapter(getApplicationContext());
+        pinAutoCompleteAdapter1.notifyDataSetChanged();
+        editText2.setAdapter(pinAutoCompleteAdapter);
+
+
+
+       /* editText.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                mPinCodeDestination = (PinCode) adapterView.getItemAtPosition(position);
+                editText.setText(String.format("%s, %s, %s", mPinCodeDestination.getLocation(), mPinCodeDestination.getPincode(), mPinCodeDestination.getState()));
+            }
+        });*/
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -61,7 +83,7 @@ public class TestDBActivity extends Activity {
         StringBuffer data = new StringBuffer();
         for (int i =0; i< list.size(); i++) {
             PinCode pin = list.get(i);
-            Log.d(TAG, pin.getLocation() + " " + pin.getPincode() + " " + pin.getState() + " " + pin.getArea());
+            //Log.d(TAG, pin.getLocation() + " " + pin.getPincode() + " " + pin.getState() + " " + pin.getArea());
             data.append(pin.getPincode()).append(",").append(pin.getLocation()).append(",").append(pin.getState()).append(",").append(pin.getArea());
         }
         textView.setText(Html.fromHtml(data.toString()));
