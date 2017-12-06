67e3e8fa5aec851e6979b418cd364b61f9883a01
Ganesh Kaple <gvklive@gmail.com>
2017-12-06 18:30:06 +0530

    Added some recyclerview customization, which aren't working yet, added crashlytics
    Signed-off-by: Ganesh Kaple <gvklive@gmail.com>

---
+++ app/src/main/java/com/scleroid/nemai/outer/Constants.java
@@ -0,0 +1,10 @@
+package com.scleroid.nemai.outer;
+
+/**
+ * Created by Ganesh on 06-12-2017.
+ */
+
+class Constants {
+    static String NAME_INNER_CONTAINER = "innerContainer";
+    String KEY_ID = "keyId";
+}
