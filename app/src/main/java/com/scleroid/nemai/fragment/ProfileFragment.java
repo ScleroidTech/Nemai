37af4c158c4a758f0cea7900337f75ca568e89cc
Ganesh Kaple <gvklive@gmail.com>
2018-01-13 15:13:50 +0530

    Finished debugging
    Signed-off-by: Ganesh Kaple <gvklive@gmail.com>

--- app/src/main/java/com/scleroid/nemai/fragment/ProfileFragment.java
+++ app/src/main/java/com/scleroid/nemai/fragment/ProfileFragment.java
@@ -78,12 +78,12 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
+       /* if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
-        }
+        }*/
     }
 
     @Override
