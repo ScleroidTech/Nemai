09edc1c97a4f03214ca979136d1cbb097d20f65d
Ganesh Kaple <gvklive@gmail.com>
2018-07-23 17:57:17 +0530

    Added serialization to the code

--- app/src/main/java/com/scleroid/nemai/network/ApiService.java
+++ app/src/main/java/com/scleroid/nemai/network/ApiService.java
@@ -1,6 +1,12 @@
 package com.scleroid.nemai.network;
 
+import com.scleroid.nemai.models.Courier;
+
+import java.util.List;
+
+import io.reactivex.Completable;
 import io.reactivex.Single;
+import retrofit2.http.Body;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -16,4 +22,11 @@ public interface ApiService {
 	@FormUrlEncoded
 	@POST("/alreadyexisteduser")
 	Single<Boolean> isAUser(@Query("email_id") String userName);
+
+	@FormUrlEncoded
+	Completable subMitCourier(@Body Courier courier);
+
+	//TODO Need to test this call first
+	@FormUrlEncoded
+	Completable subMitCouriers(@Body List<Courier> couriers);
 }
