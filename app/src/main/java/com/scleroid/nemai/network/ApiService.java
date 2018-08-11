dcf3a22626c89779a91a5defb292e3a7317e180c
Ganesh Kaple <gvklive@gmail.com>
2018-08-11 16:43:35 +0530

    Change query to field

--- app/src/main/java/com/scleroid/nemai/network/ApiService.java
+++ app/src/main/java/com/scleroid/nemai/network/ApiService.java
@@ -9,9 +9,9 @@ import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * Copyright (C) 2018
@@ -23,7 +23,7 @@ public interface ApiService {
 
 	@FormUrlEncoded
 	@POST("/alreadyexisteduser")
-	Single<Boolean> isAUser(@Query("email_id") String userName);
+	Single<Boolean> isAUser(@Field("email_id") String userName);
 
 
 	Completable subMitCourier(@Body Courier courier);
@@ -37,12 +37,14 @@ public interface ApiService {
 	Flowable<List<Courier>> getCouriers(@Body Parcel header);
 
 	@FormUrlEncoded
-	Completable registerUser(@Query("fname") String first_name, @Query("lname") String  last_name,
-	                         @Query("email") String email, @Query("phone")String phone,
-	                         @Query("gender") String gender, @Query("password") String password,
-	                         @Query("login_method") String loginMethod);
+	@POST("/register")
+	Completable registerUser(@Field("fname") String first_name, @Field("lname") String last_name,
+	                         @Field("email") String email, @Field("phone") String phone,
+	                         @Field("gender") String gender, @Field("password") String password,
+	                         @Field("login_method") String loginMethod);
 	@FormUrlEncoded
-	Single<String> loginUser(@Query("email_id") String email, @Query("password") String password);
+	@POST("/login")
+	Single<String> loginUser(@Field("email_id") String email, @Field("password") String password);
 
 
 
